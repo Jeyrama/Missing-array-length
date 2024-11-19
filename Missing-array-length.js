983143101dd0e8aa6f42d0d3b75dec0b61608504
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-18 20:22:26 -0800

    Update Missing-array-length.js

--- Missing-array-length.js
+++ Missing-array-length.js
@@ -17,3 +17,21 @@ There will always be a missing element and its length will be always between the
 
 
 // Solution
+
+function getLengthOfMissingArray(arrayOfArrays) {
+  const lengths = (arrayOfArrays || [])
+    .map(a => a ? a.length : 0)
+    .sort((a, b) => a - b)
+  
+  if (lengths.includes(0)) {
+    return 0
+  }
+
+  for (let i = 0; i < lengths.length - 1; i++) {
+    if (lengths[i] + 1 !== lengths[i + 1]) {
+      return lengths[i] + 1
+    }
+  }
+
+  return 0
+}
